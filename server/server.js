6563067289fda60de72e2a2be729b17eb18d82d9
Dev Gupta <dgupta96@rightwave.com>
2026-06-22 16:32:12 +0530

    Add Production Cors

--- server/server.js
+++ server/server.js
@@ -31,7 +31,10 @@ const PORT = process.env.PORT || 4000;
 */
 app.use(
   cors({
-    origin: "http://localhost:5173",
+     origin: [
+      "http://localhost:5173",
+      "https://signalwave-rouge.vercel.app",
+    ],
     credentials: true,
   }),
 );

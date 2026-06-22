fbf188b38802ccd67f1b72130b23f62a66406c69
Dev Gupta <dgupta96@rightwave.com>
2026-06-22 16:17:41 +0530

    Fixed Deployment Issue

--- server/services/auditService.js
+++ server/services/auditService.js
@@ -1,4 +1,4 @@
-import AuditLog from "../models/Auditlog.js";
+import AuditLog from "../models/AuditLog.js";
 
 export async function writeAuditLog({ user, action, details = {}, ipAddress }) {
   await AuditLog.create({
